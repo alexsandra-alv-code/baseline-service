e2cc86143238b43715bfd0a9eebd1f95c554aa72
Alexsandra Alvarez Vega <alexsandra.alvarez@capgemini.com>
2021-09-15 02:27:56 -0500

    Edición final del baseline-service, conexión al eureka

---
+++ src/main/java/com/tis/mx/ApplicationStarter.java
@@ -0,0 +1,22 @@
+package com.tis.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Class ApplicationStarter.
+ */
+@SpringBootApplication
+public class ApplicationStarter {
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+
+    SpringApplication.run(ApplicationStarter.class);
+  }
+
+}
